a003589d7708b8e01eb5c56bfad85377f3a273a0
Sahil Kumar <sahilkumarofficial05@gmail.com>
2024-06-16 12:38:02 +0530

    adde key

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -13,7 +13,7 @@ const TextGenerateEffect = ({text}:{text:string}) => {
         <div>
             {
                 text.split(' ').map((txt, idx) => (
-                    <span className={`${idx > 3 ? 'text-purple' : 'dark:text-white text-black'} text-center text-[40px] md:text-5xl lg:text-6xl font-bold tracking-wider leading-snug transition-[opacity] duration-500 opacity-0 ${isVisible ? `delay-${idx} opacity-100`: null}`}>{txt} </span>
+                    <span key={idx} className={`${idx > 3 ? 'text-purple' : 'dark:text-white text-black'} text-center text-[40px] md:text-5xl lg:text-6xl font-bold tracking-wider leading-snug transition-[opacity] duration-500 opacity-0 ${isVisible ? `delay-${idx} opacity-100`: null}`}>{txt} </span>
                 ))
             }
         </div>
