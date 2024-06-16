6e0b4c7d29afe9c2ced492abbed5c2881032383a
Sahil Kumar <sahilkumarofficial05@gmail.com>
2024-06-16 12:32:05 +0530

    fix perfomance

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Spotlight } from './ui/SpotLight'
-import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import TextGenerateEffect  from './ui/TextGenerateEffect'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa6'
 
 const Hero = () => {
+
+    const text = "Transforming Concepts into Seamless User Experiences"
+
     return (
-        <div className='pb-20 pt-36 bg-black-100'>
+        <div className='pb-20 pt-36 bg-black-100' id="hero">
             <div className="">
                 <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
                 <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='white' />
@@ -17,19 +20,20 @@ const Hero = () => {
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             </div>
             <div className="flex justify-center relative my-20 z-10">
-                <div className="max-w-[89vw] md:max-w-2x lg:max-w-[60vw] flex flex-col items-center justify-center">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center gap-4">
                     <h1 className='text-white uppercase tracking-widest max-w-80 text-xs'>Dynamic Web magic with next js</h1>
+                    
                     <TextGenerateEffect
-                        className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                        words='Transfoming Concepts into Seamless User Experiences'
+                        text={"Transforming Concepts into Seamless User Experiences"}
                     />
+                    
                     <p className="text-white text-center md:tracking-wider text-sm md:text-lg lg:text-2xl">
                         Hi, I&apos;m Sahil, a next js developer
                     </p>
-                    <a href="#about" className='mt-4'>
+                    <a href="#about" className=''>
                         <MagicButton
                             title="show my work"
-                            icon={<FaLocationArrow/>}
+                            icon={<FaLocationArrow />}
                             position='right'
                         />
                     </a>
--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -1,54 +1,23 @@
-"use client";
-import { useEffect } from "react";
-import { motion, stagger, useAnimate } from "framer-motion";
-import { cn } from "@/utils/cn";
+'use client'
 
-export const TextGenerateEffect = ({
-    words,
-    className,
-}: {
-    words: string;
-    className?: string;
-}) => {
-    const [scope, animate] = useAnimate();
-    let wordsArray = words.split(" ");
+import React, { useEffect, useState } from 'react'
+
+const TextGenerateEffect = ({text}:{text:string}) => {
+
+    const [isVisible, setIsVisible] = useState(false)
+
     useEffect(() => {
-        animate(
-            "span",
-            {
-                opacity: 1,
-            },
-            {
-                duration: 2,
-                delay: stagger(0.2),
-            }
-        );
-    }, [scope.current]);
-
-    const renderWords = () => {
-        return (
-            <motion.div ref={scope}>
-                {wordsArray.map((word, idx) => {
-                    return (
-                        <motion.span
-                            key={word + idx}
-                            className={`${idx > 3 ? 'text-purple' :'dark:text-white text-black'} opacity-0`}
-                        >
-                            {word}{" "}
-                        </motion.span>
-                    );
-                })}
-            </motion.div>
-        );
-    };
-
+        setIsVisible(true)
+    }, []);
     return (
-        <div className={cn("font-bold", className)}>
-            <div className="my-4">
-                <div className=" dark:text-white text-black leading-snug tracking-wide">
-                    {renderWords()}
-                </div>
-            </div>
+        <div>
+            {
+                text.split(' ').map((txt, idx) => (
+                    <span className={`${idx > 3 ? 'text-purple' : 'dark:text-white text-black'} text-center text-[40px] md:text-5xl lg:text-6xl font-bold tracking-wider leading-snug transition-[opacity] duration-500 opacity-0 ${isVisible ? `delay-${idx} opacity-100`: null}`}>{txt} </span>
+                ))
+            }
         </div>
-    );
-};
+    )
+}
+
+export default TextGenerateEffect
